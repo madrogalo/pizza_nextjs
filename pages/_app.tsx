90f7e217c6ad3eebab7cadc9737056c5e9f3d6c2
Ievhenii <youn4ik@gmail.com>
2022-05-08 22:40:29 +0300

    added redux redux-thunk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
+import { Provider } from 'react-redux'
+import { useStore } from '../store'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const store = useStore(pageProps.initialReduxState)
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
 
